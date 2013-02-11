4e63ead640be1dff94ee1f08c2e1c7c0c638860d
holypegasus <holypegasus@hotmail.com>
2013-02-10 16:32:30 -0800

    preliminary train

--- hw1.scala
+++ hw1.scala
@@ -4,6 +4,7 @@
 
 import scala.io._
 import scala.sys.process._
+import scala.math
 
 object NaiveBayes {
 
@@ -27,7 +28,25 @@ object NaiveBayes {
   /* Trains a classifer, ie computing log(P(t|c)).
    * Returns log(P(t|c)) for all t and c. */
   def train() {
-    /* comment*/
+    /* Takes in |V| x |D| matrix doc_fmat of document term frequencies */
+    num_features = doc_fmat(0).length
+    val pos_freq = fmat(0)
+    val neg_freq = fmat(1)
+    var pos_sum = 0
+    var neg_sum = 0
+    for (i <- 0 until num_features) {
+      pos_sum = pos_sum + pos_featuers(i)
+      neg_sum = neg_sum + neg_features(i)
+    }
+    /* Construct probability matrix log(P(t|c)) */
+    var pos_prob = new Array[Double](num_features)
+    var neg_prob = new Array[Double](num_features)
+    for (i <- 0 until num_features) {
+      pos_prob(i) = log( pos_freq(i) / pos_sum )
+      neg_prob(i) = log( neg_freq(i) / neg_sum )
+    }
+    val pmat = Array(pos_prob, neg_prob)
+    return pmat
   }
 
   /* Performs 10-fold cross-validation, and applies an accuracy measure. */
