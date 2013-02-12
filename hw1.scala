4d020403f3951d5e07ddef4d4a52880709c44ebd
Richard Hwang <richard.hwang201@gmail.com>
2013-02-12 13:43:11 -0800

    Class not found when trying to save sparse matrix

--- hw1.scala
+++ hw1.scala
@@ -172,9 +172,9 @@ object NaiveBayes {
       })
     }
 
-    words_docs = sparse(words_docs)
-    saveAs(word_mat_dir + word_mat_name, words_docs, "words_docs")
-    return words_docs
+    var sparse_words_docs: SMat = sparse(words_docs)
+    saveAs(word_mat_dir + word_mat_name, sparse_words_docs, "words_docs")
+    return sparse_words_docs
   }
 
   /* TODO */
@@ -182,39 +182,39 @@ object NaiveBayes {
   }
 
   /* Trains a classifer, ie computing log(P(t|c)) for all t and c. */
-  def train(docs_fmat: Array[Array[Array[Int]]]): Array[Array[Double]] = { //Takes in Integer frequency matrix; outputs Double log-probability matrix
-    /* Takes in |V| x |D| matrix doc_fmat of DOCument feature-Frequency MATrix*/
-    val pos_docs_fmat = docs_fmat(0)  //positive documents
-    val neg_docs_fmat = docs_fmat(1)  //negative documents
-    val num_features = pos_docs_fmat.length  //number of rows/features
-    val num_docs = pos_docs_fmat(0).length //number of columns/documents
-    /* Compile into two |V| x 1 matrices pos/neg_freq of aggregate feature-Frequency MATrix */
-    val pos_freq = new Array[Double](num_features)  //positive feature frequency matrix
-    val neg_freq = new Array[Double](num_features)  //negative feature frequency matrix
-    var pos_sum = 0.0 //total word count in positive documents
-    var neg_sum = 0.0 //total word count in negative documents
-    for (i <- 0 until num_features) { //iterate through rows/features
-      var pos_feature = 0.0 //word count for a particular word in positive documents
-      var neg_feature = 0.0 //word count for a particular word in negative documents
-      for (j <- 0 until num_docs) { //iterate through columns/documents
-        pos_feature += pos_doc_fmat(i)(j).toDouble //aggregate the same feature across all positive documents
-        neg_feature += neg_doc_fmat(i)(j).toDouble //aggregate the same feature across all negative documents
-      }
-      pos_freq(i) = pos_feature //populate i_th row/feature in positive document frequency matrix
-      pos_sum += pos_feature //augment total word count in positive documents
-      neg_freq(i) = neg_feature //populate i_th row/feature in negative document frequency matrix
-      neg_sum += neg_feature //augment total word count in negative documents
-    }
-    /* Construct two probability matrices for log(P(t|c)) counterpart of frequency entries */
-    var pos_prob = new Array[Double](num_features)  //positive feature log-probability matrix
-    var neg_prob = new Array[Double](num_features)  //negative feature log-probability matrix
-    for (i <- 0 until num_features) { //iterate through rows/features
-      pos_prob(i) = math.log( pos_freq(i) / pos_sum ) //compute & populate i_th row/feature in positive log-probability matrix
-      neg_prob(i) = math.log( neg_freq(i) / neg_sum ) //compute & populate i_th row/feature in negative log-probability matrix
-    }
-    val pmat = Array(pos_prob, neg_prob)  //combine positive & negative log-probability matrix into one Probability MATrix
-    return pmat  //note that output log-probabilities are negative (i.e. more frequent terms show up as less frequent terms => take absolute value before further computation?)
-  }
+  //def train(docs_fmat: Array[Array[Array[Int]]]): Array[Array[Double]] = { //Takes in Integer frequency matrix; outputs Double log-probability matrix
+  //  /* Takes in |V| x |D| matrix doc_fmat of DOCument feature-Frequency MATrix*/
+  //  val pos_docs_fmat = docs_fmat(0)  //positive documents
+  //  val neg_docs_fmat = docs_fmat(1)  //negative documents
+  //  val num_features = pos_docs_fmat.length  //number of rows/features
+  //  val num_docs = pos_docs_fmat(0).length //number of columns/documents
+  //  /* Compile into two |V| x 1 matrices pos/neg_freq of aggregate feature-Frequency MATrix */
+  //  val pos_freq = new Array[Double](num_features)  //positive feature frequency matrix
+  //  val neg_freq = new Array[Double](num_features)  //negative feature frequency matrix
+  //  var pos_sum = 0.0 //total word count in positive documents
+  //  var neg_sum = 0.0 //total word count in negative documents
+  //  for (i <- 0 until num_features) { //iterate through rows/features
+  //    var pos_feature = 0.0 //word count for a particular word in positive documents
+  //    var neg_feature = 0.0 //word count for a particular word in negative documents
+  //    for (j <- 0 until num_docs) { //iterate through columns/documents
+  //      pos_feature += pos_doc_fmat(i)(j).toDouble //aggregate the same feature across all positive documents
+  //      neg_feature += neg_doc_fmat(i)(j).toDouble //aggregate the same feature across all negative documents
+  //    }
+  //    pos_freq(i) = pos_feature //populate i_th row/feature in positive document frequency matrix
+  //    pos_sum += pos_feature //augment total word count in positive documents
+  //    neg_freq(i) = neg_feature //populate i_th row/feature in negative document frequency matrix
+  //    neg_sum += neg_feature //augment total word count in negative documents
+  //  }
+  //  /* Construct two probability matrices for log(P(t|c)) counterpart of frequency entries */
+  //  var pos_prob = new Array[Double](num_features)  //positive feature log-probability matrix
+  //  var neg_prob = new Array[Double](num_features)  //negative feature log-probability matrix
+  //  for (i <- 0 until num_features) { //iterate through rows/features
+  //    pos_prob(i) = math.log( pos_freq(i) / pos_sum ) //compute & populate i_th row/feature in positive log-probability matrix
+  //    neg_prob(i) = math.log( neg_freq(i) / neg_sum ) //compute & populate i_th row/feature in negative log-probability matrix
+  //  }
+  //  val pmat = Array(pos_prob, neg_prob)  //combine positive & negative log-probability matrix into one Probability MATrix
+  //  return pmat  //note that output log-probabilities are negative (i.e. more frequent terms show up as less frequent terms => take absolute value before further computation?)
+  //}
 
   /* Classifies the sentiment level of a given document. */
   def classify(model: Array[Array[Double]], priors: Array[Double], doc: Array[Int]): Int = {
@@ -244,41 +244,41 @@ object NaiveBayes {
   }
 
   /* Performs 10-fold cross-validation, and applies an accuracy measure. */
-  def validation(docs: String): Array[Double] = {
-    //PSEUDO-CODE//
-    /* Loop 10 times; segregate training set from test set. */
-    var test_set = 
-    var training_set = 
-    var label = //1 or -1
-    var result = 0
-    var set_correct = 0
-    var set_accuracy
-    var best_accuracy = 0.0
-    var best_other_measure = 0.0
-    var best_set = 0
-    for (i <- 0 until 10) {
-      test_set = docs(i)
-      training_set = docs - doc(i)
-      /* Process into appropriate matrices & compute model. */
-      val test_mat = 
-      val training_mat = 
-      val model = train(training_mat)
-      /* Classify & evaluate */
-      for (i <- 0 until test_set_size) {
-        result = classify(model, priors, test_mat(i))
-        if (result == label) {
-          set_correct+=1
-        }
-        set_accuracy = set_correct / test_set_size
-      }
-      if (set_accuracy > best_accuracy) {
-        best_accuracy = set_accuracy
-        best_set = i
-      }
-    }
-    /* Determine best model. */
-    return Array(best_accuracy, best_other_measure)
-  }
+  //def validation(docs: String): Array[Double] = {
+  //  //PSEUDO-CODE//
+  //  /* Loop 10 times; segregate training set from test set. */
+  //  var test_set = 
+  //  var training_set = 
+  //  var label = //1 or -1
+  //  var result = 0
+  //  var set_correct = 0
+  //  var set_accuracy
+  //  var best_accuracy = 0.0
+  //  var best_other_measure = 0.0
+  //  var best_set = 0
+  //  for (i <- 0 until 10) {
+  //    test_set = docs(i)
+  //    training_set = docs - doc(i)
+  //    /* Process into appropriate matrices & compute model. */
+  //    val test_mat = 
+  //    val training_mat = 
+  //    val model = train(training_mat)
+  //    /* Classify & evaluate */
+  //    for (i <- 0 until test_set_size) {
+  //      result = classify(model, priors, test_mat(i))
+  //      if (result == label) {
+  //        set_correct+=1
+  //      }
+  //      set_accuracy = set_correct / test_set_size
+  //    }
+  //    if (set_accuracy > best_accuracy) {
+  //      best_accuracy = set_accuracy
+  //      best_set = i
+  //    }
+  //  }
+  //  /* Determine best model. */
+  //  return Array(best_accuracy, best_other_measure)
+  //}
 
   def main(args: Array[String]) = {
     process(true)
