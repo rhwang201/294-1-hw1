ebcbabb64251d63b2cde7e1009c7947f751aa2d8
Richard Hwang <richard.hwang201@gmail.com>
2013-02-12 20:34:00 -0800

    Added f1_measure, tested preprocessing

--- hw1.scala
+++ hw1.scala
@@ -25,9 +25,12 @@ import BIDMat.Plotting._
 object NaiveBayes {
 
   val example_dir =
+    "/Users/richard/classes/294-1/hw1/review_polarity/txt_sentoken/"
+  val david_example_dir =
     "/Users/Davidius/294-1-hw1/review_polarity/txt_sentoken/"
 
-  val term_index_dir = "/Users/Davidius/294-1-hw1/"
+  val term_index_dir = "/Users/richard/classes/294-1/hw1/"
+  val david_term_index_dir = "/Users/Davidius/294-1-hw1/"
   val term_index_filename = "term_index.txt"
   val pos_files = "ls %spos".format(example_dir).!!.split("\n")
   val neg_files = "ls %sneg".format(example_dir).!!.split("\n")
@@ -42,6 +45,7 @@ object NaiveBayes {
   val priors = Array(0.5, 0.5)
   val test_set_size = 100
 
+  val n_folds = 10
 
   /* Useful. */
   def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
@@ -90,7 +94,8 @@ object NaiveBayes {
     return term_index;
   }
 
-  /* Writes a dictionary of term->index to a file. */
+  /* Writes a dictionary of term->index to a file. Was trying to attain
+   * performance improvement. */
   def create_dict_for(): mutable.Map[String,Int]  = {
     var z = 0
 
@@ -136,8 +141,9 @@ object NaiveBayes {
   }
 
   /* Returns a sparse matrix of features*/
-  def process(read_index: Boolean = false): BIDMat.SMat = {
+  def process(read_index: Boolean = false): Array[BIDMat.SMat] = {
     // Get term_index
+    val doc_mats = new Array[BIDMat.SMat](2)
     term_index = mutable.Map.empty[String, Int]
     if (read_index) {
       val s = Source.fromFile(term_index_dir + term_index_filename)
@@ -155,13 +161,14 @@ object NaiveBayes {
     val num_words = term_index.size
 
     Mat.noMKL = true
-    var words_docs: FMat = zeros(num_words, num_documents)
+    var words_pos_docs: FMat = zeros(num_words, num_documents / 2)
+    var words_neg_docs: FMat = zeros(num_words, num_documents / 2)
 
     for (i <- 0 until pos_files.length) {
       val s = Source.fromFile(example_dir + "pos/" + pos_files(i))
       s.getLines.foreach( (line) => {
         line.split("[\\s.,();:!?&\"]+").foreach({ (word: String) =>
-          words_docs(term_index(word), i) += 1
+          words_pos_docs(term_index(word), i) += 1
         })
       })
     }
@@ -169,18 +176,21 @@ object NaiveBayes {
       val s = Source.fromFile(example_dir + "neg/" + neg_files(i))
       s.getLines.foreach( (line) => {
         line.split("[\\s.,();:!?&\"]+").foreach({ (word: String) =>
-          words_docs(term_index(word), i) += 1
+          words_neg_docs(term_index(word), i) += 1
         })
       })
     }
 
-    var sparse_words_docs: SMat = sparse(words_docs)
+    var sparse_words_pos_docs: SMat = sparse(words_pos_docs)
+    var sparse_words_neg_docs: SMat = sparse(words_neg_docs)
 
     //saveAs(word_mat_dir + word_mat_name, sparse_words_docs, "words_docs")
     // Get this error:
     //   java.lang.NoClassDefFoundError: ncsa/hdf/hdf5lib/HDF5Constants
 
-    return sparse_words_docs
+    doc_mats(0) = sparse_words_pos_docs
+    doc_mats(1) = sparse_words_neg_docs
+    return doc_mats
   }
 
   /* Given a file_path, returns a binary vector of words present. */
@@ -202,13 +212,15 @@ object NaiveBayes {
   }
 
   /* Trains a classifer, ie computing log(P(t|c)) for all t and c. */
-  def train(docs_fmat: Array[Array[Array[Int]]]): Array[Array[Double]] = { //Takes in Integer frequency matrix; outputs Double log-probability matrix
+  def train(docs_fmat: Array[BIDMat.SMat]): Array[Array[Double]] = { //Takes in Integer frequency matrix; outputs Double log-probability matrix
     /* Takes in |V| x |D| matrix doc_fmat of DOCument feature-Frequency MATrix*/
-    val pos_docs_fmat = docs_fmat(0)  //positive documents
-    val neg_docs_fmat = docs_fmat(1)  //negative documents
-    val num_features = pos_docs_fmat.length  //number of rows/features
-    val num_docs = pos_docs_fmat(0).length //number of columns/documents
+    val pos_docs_fmat: BIDMat.SMat = docs_fmat(0)  //positive documents
+    val neg_docs_fmat: BIDMat.SMat = docs_fmat(1)  //negative documents
+    val num_features: Int = pos_docs_fmat.length  //number of rows/features
+    val num_docs: Int = pos_docs_fmat(0).length //number of columns/documents
+
     /* Compile into two |V| x 1 matrices pos/neg_freq of aggregate feature-Frequency MATrix */
+    // NOTE: You'll want to use BITMat matrices
     val pos_freq = new Array[Double](num_features)  //positive feature frequency matrix
     val neg_freq = new Array[Double](num_features)  //negative feature frequency matrix
     var pos_sum = 0.0 //total word count in positive documents
@@ -217,8 +229,8 @@ object NaiveBayes {
       var pos_feature = 0.0 //word count for a particular word in positive documents
       var neg_feature = 0.0 //word count for a particular word in negative documents
       for (j <- 0 until num_docs) { //iterate through columns/documents
-        pos_feature += pos_doc_fmat(i)(j).toDouble //aggregate the same feature across all positive documents
-        neg_feature += neg_doc_fmat(i)(j).toDouble //aggregate the same feature across all negative documents
+        pos_feature += pos_docs_fmat(i, j).toDouble //aggregate the same feature across all positive documents
+        neg_feature += neg_docs_fmat(i, j).toDouble //aggregate the same feature across all negative documents
       }
       pos_freq(i) = pos_feature //populate i_th row/feature in positive document frequency matrix
       pos_sum += pos_feature //augment total word count in positive documents
@@ -263,44 +275,64 @@ object NaiveBayes {
     return sent
   }
 
+  /* Returns the beta-weighted F-measure. */
+  def f_measure(precision: Double, recall: Double, beta: Int = 1):Double = {
+    return ((beta * beta + 1) * precision * recall) / (beta * beta *
+             precision * recall)
+  }
+
   /* Performs 10-fold cross-validation, and applies an accuracy measure. */
-  def validate(docs: String): Array[Double] = {
-    //PSEUDO-CODE//
+  def validate(docs: String, folds: Int = n_folds): Double = {
+    //PSEUDO-CODE
     var test_set = ""
     var training_set = ""
     var label = 0 //1 or -1
     var result = 0
-    var set_correct = 0
-    var set_accuracy = 0.0
-    var best_accuracy = 0.0
-    var best_other_measure = 0.0
-    var best_set = 0
+
+    var cur_precision = 0.0
+    var cur_recall = 0.0
+
+    var f_measure_sum = 0.0
+
     /* Loop 10 times; segregate training set from test set. */
     for (i <- 0 until 10) {
-      test_set = "" //docs(i)
+      var true_positives = 0
+      var false_positives = 0
+      var false_negatives = 0
+
+      test_set = "" //docs(i) TODO
       training_set = "" //docs - doc(i)
+
       /* Process into appropriate matrices & compute model. */
-      val test_mat = Array(1, 2, 3)
+      val test_mat = Array(1, 2, 3) // TODO
       val training_mat = Array(1, 2, 3)
       val model = train(training_mat)
-      /* Classify & evaluate */
+
+      /* Classify & sort */
       for (i <- 0 until test_set_size) {
         result = classify(model, priors, test_mat(i))
-        if (result == label) {
-          set_correct+=1
-        }
-        set_accuracy = set_correct / test_set_size
+        if (result == label && label == 1)
+          true_positives += 1
+        else if (result != label && label == 1)
+          false_positives += 1
+        else if (result != label && label == -1)
+          false_negatives += 1
       }
-      if (set_accuracy > best_accuracy) {
-        best_accuracy = set_accuracy
-        best_set = i
-      }
+
+      cur_precision = true_positives / (true_positives + false_positives)
+      cur_recall = true_positives / (true_positives + false_negatives)
+
+      f_measure_sum += f_measure(cur_precision, cur_recall, 1)
     }
-    /* Determine best model. */
-    return Array(best_accuracy, best_other_measure)
+
+    val f_measure_average = f_measure_sum / folds
+    return f_measure_average
   }
 
   def main(args: Array[String]) = {
     val mat = process(true)
+    println(mat(0)(term_index("marilyn"),0))
+    println(mat(0)(term_index("campbell"),0))
   }
+
 }
