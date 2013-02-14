0cc2792626947c0fa21457b2924b456ca8459df8
Richard Hwang <richard.hwang201@gmail.com>
2013-02-13 16:57:32 -0800

    Commeting out stemming

--- hw1.scala
+++ hw1.scala
@@ -21,7 +21,7 @@ import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
 import BIDMat.Solvers._
 import BIDMat.Plotting._
-import stemmer.Stemmer
+//import stemmer.Stemmer
 
 object NaiveBayes {
 
@@ -51,7 +51,7 @@ object NaiveBayes {
 
   var term_index = mutable.Map.empty[String, Int]
 
-  var stemmer = new Stemmer() // TODO
+  //var stemmer = new Stemmer() // TODO
 
   val num_documents = 2000
   val alpha: Double = 1.0
